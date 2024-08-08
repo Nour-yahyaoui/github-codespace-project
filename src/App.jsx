f66e0baee21873b8092e8e93801764b7b0a73b8e
Nour-Yahyaoui <nourryahyaoui@gmail.com>
2024-08-08 15:19:45 +0000

    Add folder to repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+       
+       <h1>this is a basic react app coded by Nour yahyaoui and designed with bootstrap</h1>
       </header>
+      
     </div>
   );
 }
---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './footer.css'
+
+const Footer = () => {
+  return(
+    <footer>
+        <h1>and goodbye for now !</h1>
+    </footer>
+  );  
+};
+
+export default Footer
